7b4c0126f3215eec14aa23229cd69d0a68d32ca6
ElisaTheng <elisa.theng@epitech.eu>
2018-08-13 20:30:57 +0200

    front : add toggle menu for mobile

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(function() {
 	My_ArtPaint._init();
+
+
+	// TOGGLE MENU
+	$(".aside .toggle").on("click", function() {
+		$(".aside .tools").slideToggle("normal");
+		
+		$(".aside .toggle:contains('=')").length > 0
+			? $(".aside .toggle").text("x")
+			: $(".aside .toggle").text("=");
+	});
 });
