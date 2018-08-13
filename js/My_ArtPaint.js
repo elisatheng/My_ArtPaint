64573eed8e64f1199a90ca707a3f0f36d9508c8f
ElisaTheng <elisa.theng@epitech.eu>
2018-08-13 10:10:51 +0200

    tools : pencil done

--- js/My_ArtPaint.js
+++ js/My_ArtPaint.js
@@ -49,6 +49,27 @@
 
 	_run : function() {
 		My_ArtPaint.setCanvas();
+		My_ArtPaint.getColor();
+		My_ArtPaint.getThickness();
+
+		$.each(that.tools, function(t, tbool) {
+			$("#" + t).on("click", function() {
+				My_ArtPaint.initTools();
+				that.tools[t] = true;
+
+				that.$canvas.off();
+				that.context.beginPath();
+				
+				// set color and thickness
+				that.context.lineCap = "round";
+				that.context.lineWidth = that.thickness.value;
+				that.context.strokeStyle = that.colors.stroke.value;
+
+				// painting
+				if (that.tools[t] && t == "pencil") 
+					My_ArtPaint.paintWithPencil();
+			});
+		});
 	},
 
 	setCanvas : function() {
@@ -58,6 +79,58 @@
 		$(window).resize(function() {
 			that.$canvas.css({"display":"block", "margin": "0 auto"});
 		}); 
+	},
+
+	getColor : function() {
+		$.each(that.colors, function(c, element) {
+			that.colors[c].value = that.colors[c].$element.val();
+
+			that.colors[c].$element.on("change", function() {
+				that.colors[c].value = that.colors[c].$element.val();
+			});
+		});
+	},
+
+	getThickness : function() {
+		var thickness = that.thickness;
+
+		thickness.value = thickness.$select[0].value;
+		thickness.$element.click(function() {
+			thickness.value = thickness.$select[0].value;
+		});
+	},
+
+	initTools : function() {
+		$.each(that.tools, function(t, boolean) {
+			that.tools[t] = false;
+		});
+	},
+
+	paintWithPencil: function() {
+		var mousedownActive = false;
+
+		that.$canvas.mousedown(function(e) {
+			mousedownActive = true;
+
+			that.startPoint.x = e.offsetX;
+			that.startPoint.y = e.offsetY;
+			
+			that.context.moveTo(that.startPoint.x, that.startPoint.y);
+		})
+		.mousemove(function(e) {
+			if (mousedownActive) {
+				that.context.lineTo(e.offsetX, e.offsetY);
+				that.context.stroke();
+			}
+		})
+		.mouseup(function() {
+			mousedownActive = false;
+		})
+		.mouseleave(function() {
+			that.$canvas.mouseup();
+		});
+
+		that.context.closePath();
 	},
 
 };
